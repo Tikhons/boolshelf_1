c2ac7905700834b02e6e535378c6b44993ea1f61
Tikhons <63592323+Tikhons@users.noreply.github.com>
2020-09-15 18:02:27 +0300

    Add files via upload

---
+++ bookshelf/bookshelf1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iossream>
+#indef pbook_h
+#define pbook_h
+#inlcude <string>
+#include "Book.h"
+Class book
+{
+public:
+	string Name;
+	int Pages;
+	book()
+	{
+		Nmae = '-';
+		Pages = 0;
+	}
+	string getName()
+	{
+		return Name;
+	}
+	int getPages()
+	{
+		return Pages;
+	}
+	void Print()
+	{
+		cout << "Book:" << Name << ",p." << pages << "\n"
+
+	}
+};
+
+
